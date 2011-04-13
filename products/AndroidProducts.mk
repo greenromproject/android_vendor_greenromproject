ada5cbceaa78f0970c4dc3c89ce5b7b820d915d9
Jon Stanford <jbirdvegas@gmail.com>
2011-04-13 02:10:32 -0400

    oops missing separator

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/greenromproject_sholes.mk
-    $(LOCAL_DIR)/greenromproject_desirec.mk
+    $(LOCAL_DIR)/greenromproject_sholes.mk \
+    $(LOCAL_DIR)/greenromproject_desirec.mk \
     $(LOCAL_DIR)/greenromproject_glacier.mk
