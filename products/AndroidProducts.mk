b8c1db5916bb1081852b5cf9310201dccff7a066
Jon Stanford <jbirdvegas@gmail.com>
2011-04-16 12:45:35 -0400

    still working on inc support

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/greenromproject_sholes.mk \
     $(LOCAL_DIR)/greenromproject_desirec.mk \
     $(LOCAL_DIR)/greenromproject_glacier.mk \
-    $(LOCAL_DIR)/greenromproject_harmony.mk
+    $(LOCAL_DIR)/greenromproject_harmony.mk \
+    $(LOCAL_DIR)/greenromproject_inc.mk
