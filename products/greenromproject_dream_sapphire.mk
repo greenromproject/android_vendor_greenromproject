a8f41f442c1a1a507d1897f1c5265030a8c3e709
dlamber7 <dlamber7@greenromproject.com>
2011-05-08 14:21:49 -1000

    Added greenromproject_dream_sapphire.mk and greenromproject_leo.mk

---
+++ products/greenromproject_dream_sapphire.mk
@@ -0,0 +1,61 @@
+# Inherit AOSP device configuration for dream_sapphire.
+$(call inherit-product, device/htc/dream_sapphire/full_dream_sapphire.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/greenromproject/products/common.mk)
+
+# Include GSM-only stuff
+$(call inherit-product, vendor/greenromproject/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := greenromproject_dream_sapphire
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := dream_sapphire
+PRODUCT_MODEL := Dream/Sapphire
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=tmobile/opal/sapphire/sapphire:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="opal-user 2.2.1 FRG83 60505 release-keys"
+
+# Build kernel
+#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=greenromproject_msm_defconfig
+
+# Extra DS overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/greenromproject/overlay/dream_sapphire
+
+# This file is used to install the correct audio profile when booted
+PRODUCT_COPY_FILES += \
+    vendor/greenromproject/prebuilt/dream_sapphire/etc/init.d/02audio_profile:system/etc/init.d/02audio_profile
+
+# Enable Compcache by default on D/S
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.compcache.default=18
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-DS
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-RC0-DS
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-RC0-DS-KANG
+    endif
+endif
+
+# Use the audio profile hack
+WITH_DS_HTCACOUSTIC_HACK := true
+
+#
+# Copy DS specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/greenromproject/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/greenromproject/prebuilt/dream_sapphire/etc/AudioPara_dream.csv:system/etc/AudioPara_dream.csv \
+    vendor/greenromproject/prebuilt/dream_sapphire/etc/AudioPara_sapphire.csv:system/etc/AudioPara_sapphire.csv
---
+++ products/greenromproject_leo.mk
@@ -0,0 +1,65 @@
+# Inherit AOSP device configuration for leo.
+$(call inherit-product, device/htc/leo/full_leo.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/greenromproject/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/greenromproject/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+
+PRODUCT_NAME := greenromproject_leo
+PRODUCT_BRAND := htc_grp
+PRODUCT_DEVICE := leo
+PRODUCT_MODEL := HTC HD2
+PRODUCT_MANUFACTURER := HTC
+#defined to hijack set ro.modversion routine below
+GRP_RELEASE := GoVOLS
+
+PRODUCT_BUILD_PROP_OVERRIDES:= PRODUCT_NAME=passion BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+
+# Extra leo overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/greenromproject/overlay/leo
+
+# This file is used to install the enable RMNET and corresponding modules which dont get activated by normal module script
+PRODUCT_COPY_FILES += \
+    vendor/greenromproject/prebuilt/leo/etc/init.d/01modules:system/etc/init.d/01modules \
+
+# Extra RIL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.oem.nosim.ecclist=911,112,113,115,117,999,000,08,118,120,122,110,119,995 \
+    ro.ril.emc.mode=2
+
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
+# Broadcom FM radio
+$(call inherit-product, vendor/greenromproject/products/bcm_fm_radio.mk)
+
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-LEO
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-LEO
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-LEO-KANG
+    endif
+endif
+
+
+#
+# Copy leo specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/greenromproject/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
