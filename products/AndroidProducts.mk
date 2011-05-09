143634ef243f54ab398ddf9e801b15d3d32d947c
JBirdVegas <jbirdvegas@gmail.com>
2011-05-09 02:21:28 -0400

    oops! now we can build for leo and dream_sapphire

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,4 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/greenromproject_desirec.mk \
     $(LOCAL_DIR)/greenromproject_glacier.mk \
     $(LOCAL_DIR)/greenromproject_harmony.mk \
-    $(LOCAL_DIR)/greenromproject_inc.mk
+    $(LOCAL_DIR)/greenromproject_inc.mk \
+    $(LOCAL_DIR)/greenromproject_leo \
+    $(LOCAL_DIR)/greenromproject_dream_sapphire
+
