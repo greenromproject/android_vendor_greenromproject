db04c311ef2233e1eb90a5794ecfda8d05f7b1c3
JBirdVegas <jbirdvegas@gmail.com>
2011-05-13 14:46:51 -0400

    excluded gapps

--- products/greenromproject.mk
+++ products/greenromproject.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # GreenRomProject apps 
 PRODUCT_COPY_FILES += \
 	vendor/greenromproject/grpapps/Launcher2.apk:system/app/Launcher2.apk \
-	vendor/greenromproject/grpapps/greenromproject.apk:system/app/greenromproject.apk \
+	#vendor/greenromproject/grpapps/greenromproject.apk:system/app/greenromproject.apk \
 	vendor/greenromproject/grpapps/90ramhack:system/etc/init.d/90ramhack \
 	vendor/greenromproject/grpapps/95speedhacks:system/etc/init.d/95speedhacks \
 	vendor/greenromproject/grpapps/98chmodbomb:system/etc/init.d/98chmodbomb \
@@ -32,20 +32,24 @@ PRODUCT_COPY_FILES += \
 # MISC included apps
 PRODUCT_COPY_FILES += \
         vendor/greenromproject/proprietary/Gmail.apk:./system/app/Gmail.apk \
-        vendor/greenromproject/proprietary/GoogleBackupTransport.apk:./system/app/GoogleBackupTransport.apk \
-        vendor/greenromproject/proprietary/GoogleCalendarSyncAdapter.apk:./system/app/GoogleCalendarSyncAdapter.apk \
-        vendor/greenromproject/proprietary/GoogleContactsSyncAdapter.apk:./system/app/GoogleContactsSyncAdapter.apk \
-        vendor/greenromproject/proprietary/GoogleFeedback.apk:./system/app/GoogleFeedback.apk \
-        vendor/greenromproject/proprietary/GooglePartnerSetup.apk:./system/app/GooglePartnerSetup.apk \
-        vendor/greenromproject/proprietary/GoogleQuickSearchBox.apk:./system/app/GoogleQuickSearchBox.apk \
-        vendor/greenromproject/proprietary/GoogleServicesFramework.apk:./system/app/GoogleServicesFramework.apk \
-        vendor/greenromproject/proprietary/LatinImeTutorial.apk:./system/app/LatinImeTutorial.apk \
-        vendor/greenromproject/proprietary/MarketUpdater.apk:./system/app/MarketUpdater.apk \
-        vendor/greenromproject/proprietary/MediaUploader.apk:./system/app/MediaUploader.apk \
-        vendor/greenromproject/proprietary/NetworkLocation.apk:./system/app/NetworkLocation.apk \
         vendor/greenromproject/proprietary/OneTimeInitializer.apk:./system/app/OneTimeInitializer.apk \
         vendor/greenromproject/proprietary/SetupWizard.apk:./system/app/SetupWizard.apk \
         vendor/greenromproject/proprietary/com.google.android.maps.xml:./system/etc/permissions/com.google.android.maps.xml \
         vendor/greenromproject/proprietary/features.xml:./system/etc/permissions/features.xml \
         vendor/greenromproject/proprietary/com.google.android.maps.jar:./system/framework/com.google.android.maps.jar \
         vendor/greenromproject/proprietary/libvoicesearch.so:./system/lib/libvoicesearch.so
+
+# EXCLUDED gapps
+#
+#        vendor/greenromproject/proprietary/GoogleBackupTransport.apk:./system/app/GoogleBackupTransport.apk \
+#        vendor/greenromproject/proprietary/GoogleCalendarSyncAdapter.apk:./system/app/GoogleCalendarSyncAdapter.apk \
+#        vendor/greenromproject/proprietary/GoogleContactsSyncAdapter.apk:./system/app/GoogleContactsSyncAdapter.apk \
+#        vendor/greenromproject/proprietary/GoogleFeedback.apk:./system/app/GoogleFeedback.apk \
+#        vendor/greenromproject/proprietary/GooglePartnerSetup.apk:./system/app/GooglePartnerSetup.apk \
+#        vendor/greenromproject/proprietary/GoogleQuickSearchBox.apk:./system/app/GoogleQuickSearchBox.apk \
+#        vendor/greenromproject/proprietary/GoogleServicesFramework.apk:./system/app/GoogleServicesFramework.apk \
+#        vendor/greenromproject/proprietary/LatinImeTutorial.apk:./system/app/LatinImeTutorial.apk \
+#        vendor/greenromproject/proprietary/MarketUpdater.apk:./system/app/MarketUpdater.apk \
+#        vendor/greenromproject/proprietary/MediaUploader.apk:./system/app/MediaUploader.apk \
+#        vendor/greenromproject/proprietary/NetworkLocation.apk:./system/app/NetworkLocation.apk \
+
